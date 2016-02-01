03986ee13e531c090e8d0e1efa19c34a81958799
Pavel Ivanov <mitis74@yandex.ru>
2016-02-01 16:44:49 +0500

    Add code for pushes

--- GpsPing/AppDelegate.h
+++ GpsPing/AppDelegate.h
@@ -7,11 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Google/CloudMessaging.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property(nonatomic, readonly, strong) NSString *registrationKey;
+@property(nonatomic, readonly, strong) NSString *messageKey;
+@property(nonatomic, readonly, strong) NSString *gcmSenderID;
+@property(nonatomic, readonly, strong) NSDictionary *registrationOptions;
 
 @end
 
--- GpsPing/AppDelegate.m
+++ GpsPing/AppDelegate.m
@@ -20,6 +20,9 @@
 DDLogLevel ddLogLevel = DDLogLevelError;
 
 @interface AppDelegate ()
+@property(nonatomic, strong) void (^registrationHandler)
+(NSString *registrationToken, NSError *error);
+@property(nonatomic, strong) NSString* registrationToken;
 
 @end
 
@@ -50,6 +53,8 @@ DDLogLevel ddLogLevel = DDLogLevelError;
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    _registrationKey = @"onRegistrationCompleted";
+    _messageKey = @"onMessageReceived";
     // Override point for customization after application launch.
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.5451 green:0.7647 blue:0.2902 alpha:1.0]];
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
@@ -59,6 +64,47 @@ DDLogLevel ddLogLevel = DDLogLevelError;
             NSFontAttributeName: [UIFont fontWithName:@"Roboto-Regular" size:20.0f]
         }];
     [self setDefaultTrackDuration];
+    
+    NSError* configureError;
+    [[GGLContext sharedInstance] configureWithError:&configureError];
+    NSAssert(!configureError, @"Error configuring Google services: %@", configureError);
+    _gcmSenderID = [[[GGLContext sharedInstance] configuration] gcmSenderID];
+
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationType types = UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+        UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [application registerUserNotificationSettings:notificationSettings];
+        [application registerForRemoteNotifications];
+    } else {
+        [application registerForRemoteNotificationTypes:
+         UIRemoteNotificationTypeSound|
+         UIRemoteNotificationTypeAlert];
+    }
+    
+    GCMConfig *gcmConfig = [GCMConfig defaultConfig];
+    gcmConfig.receiverDelegate = self;
+    [[GCMService sharedInstance] startWithConfig:gcmConfig];
+    // [END start_gcm_service]
+    __weak typeof(self) weakSelf = self;
+    // Handler for registration token request
+    _registrationHandler = ^(NSString *registrationToken, NSError *error){
+        if (registrationToken != nil) {
+            weakSelf.registrationToken = registrationToken;
+            NSLog(@"Registration Token: %@", registrationToken);
+//            [weakSelf subscribeToTopic];
+            NSDictionary *userInfo = @{@"registrationToken":registrationToken};
+            [[NSNotificationCenter defaultCenter] postNotificationName:weakSelf.registrationKey
+                                                                object:nil
+                                                              userInfo:userInfo];
+        } else {
+            NSLog(@"Registration to GCM failed with error: %@", error.localizedDescription);
+            NSDictionary *userInfo = @{@"error":error.localizedDescription};
+            [[NSNotificationCenter defaultCenter] postNotificationName:weakSelf.registrationKey
+                                                                object:nil
+                                                              userInfo:userInfo];
+        }
+    };
+
     return YES;
 }
 
@@ -96,5 +142,87 @@ DDLogLevel ddLogLevel = DDLogLevelError;
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+//    [self registerNotificationToken:deviceToken];
+    //    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    //    [notificationCenter postNotificationName:kAGPushNotificationRegistred object:nil userInfo:nil];
+    NSString *uid = [[UIDevice currentDevice] identifierForVendor].UUIDString;
+    // Create a config and set a delegate that implements the GGLInstaceIDDelegate protocol.
+    GGLInstanceIDConfig *instanceIDConfig = [GGLInstanceIDConfig defaultConfig];
+    instanceIDConfig.delegate = self;
+    // Start the GGLInstanceID shared instance with the that config and request a registration
+    // token to enable reception of notifications
+    [[GGLInstanceID sharedInstance] startWithConfig:instanceIDConfig];
+    _registrationOptions = @{kGGLInstanceIDRegisterAPNSOption:deviceToken,
+                             kGGLInstanceIDAPNSServerTypeSandboxOption:@YES};
+    [[GGLInstanceID sharedInstance] tokenWithAuthorizedEntity:_gcmSenderID
+                                                        scope:kGGLInstanceIDScopeGCM
+                                                      options:_registrationOptions
+                                                      handler:_registrationHandler];
+}
+
+//- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
+//    DDLogError(@"Error in registration for push. Error: %@", err);
+////    [[[CustomAlertView alloc] initWithTitle:@"Ошибка при регистрации для нотификаций" error:err] show];
+//    //    self.apiController.deviceTokenForPush = nil;
+//    //    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+//    //    [notificationCenter postNotificationName:kAGPushNotificationFailedToRegister object:nil userInfo:nil];
+//}
+
+- (void)application:(UIApplication *)application
+didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"Registration for remote notification failed with error: %@", error.localizedDescription);
+    // [END receive_apns_token_error]
+    NSDictionary *userInfo = @{@"error" :error.localizedDescription};
+    [[NSNotificationCenter defaultCenter] postNotificationName:_registrationKey
+                                                        object:nil
+                                                      userInfo:userInfo];
+}
+
+// [START ack_message_reception]
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"Notification received: %@", userInfo);
+    // This works only if the app started the GCM service
+    [[GCMService sharedInstance] appDidReceiveMessage:userInfo];
+    // Handle the received message
+    // [START_EXCLUDE]
+    [[NSNotificationCenter defaultCenter] postNotificationName:_messageKey
+                                                        object:nil
+                                                      userInfo:userInfo];
+    // [END_EXCLUDE]
+}
+
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))handler {
+    NSLog(@"Notification received: %@", userInfo);
+    // This works only if the app started the GCM service
+    [[GCMService sharedInstance] appDidReceiveMessage:userInfo];
+    // Handle the received message
+    // Invoke the completion handler passing the appropriate UIBackgroundFetchResult value
+    // [START_EXCLUDE]
+    [[NSNotificationCenter defaultCenter] postNotificationName:_messageKey
+                                                        object:nil
+                                                      userInfo:userInfo];
+    handler(UIBackgroundFetchResultNoData);
+    // [END_EXCLUDE]
+}
+
+-(void) registerNotificationToken: (NSData *) deviceToken {
+//    NSString* tokenString = [deviceToken.description stringByReplacingOccurrencesOfString:@" " withString:@""];
+//    tokenString = [tokenString stringByReplacingOccurrencesOfString:@"<" withString:@""];
+//    tokenString = [tokenString stringByReplacingOccurrencesOfString:@">" withString:@""];
+    
+//    self.apiController.deviceTokenForPush = tokenString;
+//    [[self.apiController setNewPushId:tokenString] subscribeNext:^(id x) {
+//        DDLogDebug(@"Push notificaiton setted up. Token = %@", tokenString);
+//    } error:^(NSError *error) {
+//        DDLogError(@"Cannot set push notificaiton token. Error: %@", error);
+//    }];
+}
+
 
 @end
