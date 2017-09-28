3e20b65d5334108a7aafb7d1244e6eea7e80aa91
harbinger <iwannagonnafly@gmail.com>
2017-09-29 00:16:05 +0500

    append white details #0

---
+++ GpsPing/ASWhiteMapDetailsView.h
@@ -0,0 +1,53 @@
+//
+//  ASWhiteMapDetailsView.h
+//  GpsPing
+//
+//  Created by Юджин Топсекретович on 9/28/17.
+//  Copyright © 2017 Robin Grønvold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ASModel.h"
+@interface ASWhiteMapDetailsView : UIView
+
+@property (nonatomic) IBOutlet UILabel *labelTrackerName;
+//@property (nonatomic) IBOutlet UILabel *labelOwnerName;
+//@property (nonatomic) IBOutlet UIImageView *imaeViewOwnerIcon;
+
+//@property (nonatomic) IBOutlet UILabel *labelTrackerNumber;
+//@property (nonatomic) IBOutlet UILabel *labelImei;
+@property (nonatomic) IBOutlet UILabel *labelLogTime;
+
+//@property (nonatomic) IBOutlet UILabel *labelLatitude;
+//@property (nonatomic) IBOutlet UILabel *labelLongitude;
+@property (nonatomic) IBOutlet UILabel *labelGrsm;
+@property (nonatomic) IBOutlet UILabel *labelDistance;
+@property (nonatomic) IBOutlet UILabel *labelDistanceTravelled;
+
+
+@property (nonatomic) IBOutlet UILabel *labelBatteryLevel;
+@property (nonatomic) IBOutlet UIImageView *imageViewBattery;
+
+@property (nonatomic) IBOutlet UIImageView *imageViewGSM;
+@property (nonatomic) IBOutlet UIImageView *imageViewGPS;
+@property (nonatomic) IBOutlet UIButton *btnMap;
+@property (nonatomic) IBOutlet UIButton *btnEdit;
+
+//@property (nonatomic) IBOutlet UILabel *labelPOILatitude;
+//@property (nonatomic) IBOutlet UILabel *labelPOILongitude;
+//@property (nonatomic) IBOutlet UILabel *labelPOIGrsm;
+
+//@property (nonatomic) IBOutlet UIView *viewLeftColumn;
+//@property (nonatomic) IBOutlet UIView *viewRightColumn;
+//@property (nonatomic) IBOutlet UIView *viewPOILeftColumn;
+//@property (nonatomic) IBOutlet UIView *viewPOIRightColumn;
+
+-(void)configWithOwner:(ASFriendModel*)owner
+               tracker:(ASDeviceModel*)deviceModel
+                 point:(ASPointModel*)pointModel
+                 color:(UIColor*)color;
+-(void)configWithPOI:(ASPointOfInterestModel*)poi
+           withOwner:(ASFriendModel*)owner
+               color:(UIColor*)color;
+
+@end
---
+++ GpsPing/ASWhiteMapDetailsView.m
@@ -0,0 +1,22 @@
+//
+//  ASWhiteMapDetailsView.m
+//  GpsPing
+//
+//  Created by Юджин Топсекретович on 9/28/17.
+//  Copyright © 2017 Robin Grønvold. All rights reserved.
+//
+
+#import "ASWhiteMapDetailsView.h"
+
+@implementation ASWhiteMapDetailsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+@end
