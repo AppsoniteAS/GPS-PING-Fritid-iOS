ed2d59451f8afeaf49191c9c8dc32b2e485034bd
harbinger <iwannagonnafly@gmail.com>
2017-10-19 19:19:18 +0500

    append rotation to pins

--- GpsPing/UIImage+ASAnnotations.m
+++ GpsPing/UIImage+ASAnnotations.m
@@ -5,10 +5,39 @@
 //  Created by Pavel Ivanov on 28/01/16.
 //  Copyright © 2016 Robin Grønvold. All rights reserved.
 //
+@import CoreImage;
+@import CoreGraphics;
+
 
 #import "UIImage+ASAnnotations.h"
 
 @implementation UIImage (ASAnnotations)
+
+static CGFloat DegreesToRadians(CGFloat degrees) {return degrees * M_PI / 180;};
+
+- (UIImage *)imageRotatedByDegrees:(CGFloat)degrees
+{
+    UIView *rotatedViewBox = [[UIView alloc] initWithFrame:CGRectMake(0,0,self.size.width, self.size.height)];
+    CGAffineTransform t = CGAffineTransformMakeRotation(DegreesToRadians(degrees));
+    rotatedViewBox.transform = t;
+    CGSize rotatedSize = rotatedViewBox.frame.size;
+    
+    UIGraphicsBeginImageContextWithOptions(rotatedSize, NO, 0.0);
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    
+    CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
+    
+    CGContextRotateCTM(bitmap, DegreesToRadians(degrees));
+    
+    CGContextScaleCTM(bitmap, 1.0, -1.0);
+    CGContextDrawImage(bitmap, CGRectMake(-self.size.width / 2, -self.size.height / 2, self.size.width, self.size.height), [self CGImage]);
+    
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+
 
 +(UIImage*)getUserAnnotationImageWithColor:(UIColor*)color
 {
@@ -24,7 +53,20 @@
 }
 
 +(UIImage*)getLastPointAnnotationImageWithColorName:(NSString*) name andRotation: (CGFloat) rotation{
-    return [UIImage imageNamed: [NSString stringWithFormat: @"direction-%@", name ]];
+    UIImage* im =  [UIImage imageNamed: [NSString stringWithFormat: @"direction-%@", name ]];
+
+    return [im imageRotatedByDegrees:90];
+}
+
+- (UIImage*)rotateUIImage:(UIImage*)sourceImage clockwise:(BOOL)clockwise
+{
+    CGSize size = sourceImage.size;
+    UIGraphicsBeginImageContext(CGSizeMake(size.height, size.width));
+    [[UIImage imageWithCGImage:[sourceImage CGImage] scale:1.0 orientation:clockwise ? UIImageOrientationRight : UIImageOrientationLeft] drawInRect:CGRectMake(0,0,size.height ,size.width)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 +(UIImage*)getPointAnnotationImageWithColorName:(NSString*) name andRotation: (CGFloat) rotation{
