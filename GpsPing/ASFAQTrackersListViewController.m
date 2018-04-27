1b29da5dab557c08b73f2b29e87d38ce9ab6e022
Евгений Якубович <iwannagonnafly@gmail.com>
2018-04-27 20:15:32 +0500

    FAQ --> Marcel Tracker --> open safari

--- GpsPing/ASFAQTrackersListViewController.m
+++ GpsPing/ASFAQTrackersListViewController.m
@@ -12,6 +12,7 @@
 #import "ASTrackerModel.h"
 #import "UIStoryboard+ASHelper.h"
 #import "ASFAQTrackerListCell.h"
+@import SafariServices;
 
 @interface ASFAQTrackersListViewController ()<UITableViewDelegate, UITableViewDataSource,ASFAQTrackerListCellProtocol>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -68,9 +69,28 @@
 #pragma mark - ASFAQTrackerListCellProtocol
 
 - (void)didSelectTracker:(NSString *)trackerName{
+    if ([trackerName isEqualToString: @"S1"]){
+        [self openLink:@"https://fritid.gpsping.no/brukerveiledning_marcel/"];
+        return;
+    }
     ASQuestionListTableViewController* trackerController = [[UIStoryboard faqStoryboard] instantiateViewControllerWithIdentifier:@"ASQuestionListTableViewController"];
     trackerController.trackerType = trackerName;
     [self.navigationController pushViewController: trackerController animated:YES];
+}
+
+- (void)openLink:(NSString *)url {
+    NSURL *URL = [NSURL URLWithString:url];
+    
+    if (URL) {
+        if ([SFSafariViewController class] != nil) {
+            SFSafariViewController *sfvc = [[SFSafariViewController alloc] initWithURL:URL];
+            [self presentViewController:sfvc animated:YES completion:nil];
+        } else {
+            if (![[UIApplication sharedApplication] openURL:url]) {
+                NSLog(@"%@%@",@"Failed to open url:",[url description]);
+            }
+        }
+    } 
 }
 
 @end
